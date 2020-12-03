c3b976a434cd9bfe211f6d0849595510a68ae1e9
wickedr0se <32614215+wickedr0se@users.noreply.github.com>
2020-12-03 21:54:04 +0700

    upload to db

--- upload.php
+++ upload.php
@@ -1,16 +1,27 @@
 <?php
+require_once "config.php";
 $target_dir = "upload/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+$target_file = explode(".", $_FILES["fileToUpload"]["name"]);
+$newfilename = round(microtime(true)) . '.' . end($target_file);
 
 if(isset($_POST["submit"])) {
-	if (file_exists($target_file)) {
-		echo "Sorry, file already exists.";
-		$uploadOk = 0;
-	}
-	else if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+	if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file . $newfilename)) {
 		echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+		$sql = "INSERT INTO comp (name, email, phone, complaint, file) VALUES (?, ?, ?, ?, ?)";
+		if($stmt = mysqli_prepare($link, $sql)){
+            mysqli_stmt_bind_param($stmt, "sssss", $name, $email, $phone, $desc, $file);
+			$name = trim($_POST["name"]);
+			$email = trim($_POST["email"]);
+			$phone = trim($_POST["phone"]);
+			$desc = trim($_POST["desc"]);
+			$file = trim($_FILES["fileToUpload"]["name"]);
+            if(mysqli_stmt_execute($stmt)){
+                header("location: index.php");
+            } else{
+                echo "Something went wrong. Please try again later.";
+            }
+            mysqli_stmt_close($stmt);
+        }
 	} 
 	else {
 		echo "Sorry, there was an error uploading your file.";
