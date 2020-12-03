91d2e654c02dea9af3923cd0e8e46b563683031d
wirahmat <58240412+wirahmat@users.noreply.github.com>
2020-12-03 22:54:02 +0700

    customer care(upload file) redesign

--- upload.php
+++ upload.php
@@ -5,7 +5,8 @@ $target_file = explode(".", $_FILES["fileToUpload"]["name"]);
 $newfilename = round(microtime(true)) . '.' . end($target_file);
 
 if(isset($_POST["submit"])) {
-	if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file . $newfilename)) {
+	if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_dir . $newfilename)) {
+        
 		echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
 		$sql = "INSERT INTO comp (name, email, phone, complaint, file) VALUES (?, ?, ?, ?, ?)";
 		if($stmt = mysqli_prepare($link, $sql)){
@@ -14,13 +15,15 @@ if(isset($_POST["submit"])) {
 			$email = trim($_POST["email"]);
 			$phone = trim($_POST["phone"]);
 			$desc = trim($_POST["desc"]);
-			$file = trim($_FILES["fileToUpload"]["name"]);
+			$file = trim($newfilename);
             if(mysqli_stmt_execute($stmt)){
-                header("location: index.php");
+                echo "<script>alert('Your Complaint is Received')
+                window.location.href='index.php'</script>";
             } else{
                 echo "Something went wrong. Please try again later.";
             }
             mysqli_stmt_close($stmt);
+//            header("location: index.php");
         }
 	} 
 	else {
